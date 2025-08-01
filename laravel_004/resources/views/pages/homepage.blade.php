e5ed40d390fd851d646f1ad94178f4d3c07dc6fc
Arwel219 <aruelangeloriv01@gmail.com>
2025-08-01 13:03:25 +0200

    modifiche adattate sulla homepage

--- laravel_004/resources/views/pages/homepage.blade.php
+++ laravel_004/resources/views/pages/homepage.blade.php
@@ -17,7 +17,7 @@
                      <img src="{{ isset($film->cover) ? Storage::url($film->cover) : 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDI1bWNuZWJlcXBxcWd1cWttMjhlaWdpaXcwcHlhbWNoNnAyc2N1aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/AwJuJzp1pGrcfO51g6/giphy.gif' }}"
                          class="card-img-top" alt="...">
                      <div class="card-body">
-                         <h5 class="card-title">{{ $film->name }}</h5>
+                         <h5 class="card-title">{{ $film->title }}</h5>
                          <p class="card-text">{{ $film->description }}</p>
                      </div>
                  </div>
